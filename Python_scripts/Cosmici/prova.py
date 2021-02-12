8a06cac0c6ec5d968573e752ffceb4a94f17f599
marcomonte94 <marco.montefiori94@gmail.com>
2021-02-12 09:14:36 +0100

    Last commit

--- Python_scripts/Cosmici/prova.py
+++ Python_scripts/Cosmici/prova.py
@@ -1,9 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from new_channelmap import channelMap
 
-dt = np.dtype([('channel', np.int), ('id_board', np.int), ('id_event', np.int), ('always_0_1', np.int), ('always_0_2', np.int), ('ampl', np.float, (1024, )), ('time', np.float, (1024,))])
+dt = np.dtype([
+    ('channel',         np.int),
+    ('id_board',        np.int),
+    ('id_event',        np.int),
+    ('always_0_1',      np.int),
+    ('always_0_2',      np.int),
+    ('ampl',            np.float, (1024, )),
+    ('time',            np.float, (1024, ))
+  ])
 
 path = 'C:/Users/Marco/Desktop/cosmic/prova.dec'
 a = np.fromfile(path, dtype=dt)
-plt.plot(a[100]['time'],a[100]['ampl'])
-plt.show()
+e = channelMap(a)
+mask = np.logical_and(e['id_bar']==0, e['layer']==0)
+print(len(e[mask]))
+
+
+
+
+
+
+
+
+
+
+
+
